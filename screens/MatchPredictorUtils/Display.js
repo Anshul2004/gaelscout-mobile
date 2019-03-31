1a4dc1a3d3a9182d59192b329f89d81ecf5abcf1
Anshul <Anshul@anshuls-mbp.attlocal.net>
2019-03-30 19:00:21 -0700

    Finished MatchPredictor Display page

--- screens/MatchPredictorUtils/Display.js
+++ screens/MatchPredictorUtils/Display.js
@@ -8,27 +8,28 @@ export default class Display extends React.Component {
 	}
 	componentDidMount() {
 		const { navigation } = this.props;
+
 		const rating1 = navigation.getParam('red1Rating', "nodata");
 		const rating2 = navigation.getParam('red2Rating', "nodata");
 		const rating3 = navigation.getParam('blue1Rating', "nodata");
 		const rating4 = navigation.getParam('blue2Rating', "nodata");
 
-		const val = (280-(getOutput([rating1, rating2, rating3, rating4])*280))+80;
-		const val2 = ((getOutput([rating1, rating2, rating3, rating4])*280))+80;
+		const val = (200-(getOutput([rating1, rating2, rating3, rating4])*200))+100;
+		const val2 = ((getOutput([rating1, rating2, rating3, rating4])*200))+100;
 
 		Animated.parallel([
 			Animated.timing(
 				this.state.blueAnim,
 				{
 				  toValue: val,
-				  duration: 1000,
+				  duration: 500,
 				}
 			).start(),
 			Animated.timing(
 				this.state.redAnim,
 				{
 				  toValue: val2,
-				  duration: 1000,
+				  duration: 500,
 				}
 			).start()  
 		]);
@@ -39,6 +40,11 @@ export default class Display extends React.Component {
 
 		const { navigation } = this.props;
 
+		const rating1 = navigation.getParam('red1Rating', "nodata");
+		const rating2 = navigation.getParam('red2Rating', "nodata");
+		const rating3 = navigation.getParam('blue1Rating', "nodata");
+		const rating4 = navigation.getParam('blue2Rating', "nodata");
+
 		const red1 = navigation.getParam('red1Name', "nodata");
 		const red2 = navigation.getParam('red2Name', "nodata");
 		const blue1 = navigation.getParam('blue1Name', "nodata");
@@ -46,15 +52,18 @@ export default class Display extends React.Component {
 
 		return (
 			<View style={styles.container}>
+				<Text style={styles.title}>Winning Percentages</Text>
 				<Animated.View style={[styles.blueView, {...this.props.style, height:blueAnim}]}>
 					<Text style={styles.text}>{blue1}</Text>
+					<Text style={styles.textPercent}>{Number(((1-getOutput([rating1, rating2, rating3, rating4]))*100).toFixed(1))}%</Text>
 					<Text style={styles.text}>{blue2}</Text>
 					{this.props.children}
 				</Animated.View>
 				<Text style={styles.versus}>VS</Text>
 				<Animated.View style={[styles.redView, {...this.props.style, height:redAnim}]}>
 					<Text style={styles.text}>{red1}</Text>
-					<Text style={[styles.text, {marginTop:20}]}>{red2}</Text>
+					<Text style={styles.textPercent}>{Number(((getOutput([rating1, rating2, rating3, rating4]))*100).toFixed(1))}%</Text>
+					<Text style={[styles.text]}>{red2}</Text>
 				</Animated.View>
 			</View>
 		);
@@ -85,7 +94,7 @@ const styles = StyleSheet.create({
 		width: 300,
 		backgroundColor: "#42b3f4",
 		borderRadius: 20,
-		marginTop: 20
+		marginTop: 20,
 	},
 	redView: {
 		width: 300,
@@ -94,14 +103,26 @@ const styles = StyleSheet.create({
 	},
 	text:{
 		color: "#efefef",
-		fontSize: 50,
+		fontSize: 40,
 		marginTop: 10,
 		fontWeight: "200",
-		marginLeft: 20
+		marginLeft: 20,
+		flex: 1,
+    	justifyContent: 'center',
+    	alignItems: 'center',
+    	flexDirection: 'row',
+	},
+	textPercent:{
+		color: "#efefef",
+		fontSize: 40,
+		fontWeight: "300",
+		marginLeft: 180,
+		marginTop:-20,
+		marginBottom:-20
 	},
 	versus:{
 		color: "#474747",
-		fontSize: 50,
+		fontSize: 30,
 		zIndex: 5,
 		fontWeight: "300",
 		paddingTop: 10,
